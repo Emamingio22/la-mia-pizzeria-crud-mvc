97d3cf47eea8835854e6a0b6f74a75584e168560
emamingio22 <emawankenoby02@gmail.com>
2023-05-17 14:16:12 +0200

    Aggiungere i file di progetto.

---
+++ LaMiaPizzeria/Models/Pizze.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaMiaPizzeria.Models
+{
+    public class Pizze
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+
+        public Pizze(int id, string image, string name, string description, int price)
+        {
+            id = Id;
+            image = Image;
+
+        }
+
+    }
+}
